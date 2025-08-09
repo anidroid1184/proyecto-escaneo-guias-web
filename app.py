fc1a5b856e0fd0dc0c9beb9258acad18d5d85873
Juan Sebastian Valencia Londoño <valencialondonojuansebastian@gmail.com>
2025-08-08 21:52:05 -0500

    Arreglos para servidor en anywhere

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 os.makedirs(app.config['EXPORT_FOLDER'], exist_ok=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=8000)
